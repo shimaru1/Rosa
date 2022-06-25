84a174ed834253cdcd54f6871bde82dfe1d1b8e9
Gustavo_Gomes <103595240+shimaru1@users.noreply.github.com>
2022-06-25 18:01:31 -0300

    Add files via upload

---
+++ Rosa/Rosa/Rosa/cadastrar.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Rosa
+{
+    partial class Cadastrar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cadastrar));
+            this.MenoPausaBox = new System.Windows.Forms.TextBox();
+            this.MenarcaBox = new System.Windows.Forms.TextBox();
+            this.DataBox = new System.Windows.Forms.TextBox();
+            this.NomeBox = new System.Windows.Forms.TextBox();
+            this.CodigoBox = new System.Windows.Forms.TextBox();
+            this.codigo = new System.Windows.Forms.Label();
+            this.menopausa = new System.Windows.Forms.Label();
+            this.menarca = new System.Windows.Forms.Label();
+            this.Peso = new System.Windows.Forms.Label();
+            this.dataN = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.CadastroRosa = new System.Windows.Forms.Label();
+            this.Atualizar = new System.Windows.Forms.Button();
+            this.consultar = new System.Windows.Forms.Button();
+            this.ciclo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PesoBox = new System.Windows.Forms.MaskedTextBox();
+            this.BtVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MenoPausaBox
+            // 
+            this.MenoPausaBox.Location = new System.Drawing.Point(738, 267);
+            this.MenoPausaBox.Name = "MenoPausaBox";
+            this.MenoPausaBox.Size = new System.Drawing.Size(272, 20);
+            this.MenoPausaBox.TabIndex = 25;
+            this.MenoPausaBox.TextChanged += new System.EventHandler(this.MenoPausaBox_TextChanged);
+            // 
+            // MenarcaBox
+            // 
+            this.MenarcaBox.Location = new System.Drawing.Point(738, 226);
+            this.MenarcaBox.Name = "MenarcaBox";
+            this.MenarcaBox.Size = new System.Drawing.Size(273, 20);
+            this.MenarcaBox.TabIndex = 24;
+            this.MenarcaBox.TextChanged += new System.EventHandler(this.MenarcaBox_TextChanged);
+            // 
+            // DataBox
+            // 
+            this.DataBox.Location = new System.Drawing.Point(738, 151);
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(272, 20);
+            this.DataBox.TabIndex = 22;
+            this.DataBox.TextChanged += new System.EventHandler(this.DataBox_TextChanged);
+            // 
+            // NomeBox
+            // 
+            this.NomeBox.Location = new System.Drawing.Point(738, 107);
+            this.NomeBox.Name = "NomeBox";
+            this.NomeBox.Size = new System.Drawing.Size(272, 20);
+            this.NomeBox.TabIndex = 21;
+            this.NomeBox.TextChanged += new System.EventHandler(this.NomeBox_TextChanged);
+            // 
+            // CodigoBox
+            // 
+            this.CodigoBox.Location = new System.Drawing.Point(738, 67);
+            this.CodigoBox.Name = "CodigoBox";
+            this.CodigoBox.Size = new System.Drawing.Size(272, 20);
+            this.CodigoBox.TabIndex = 20;
+            this.CodigoBox.TextChanged += new System.EventHandler(this.CodigoBox_TextChanged);
+            // 
+            // codigo
+            // 
+            this.codigo.AutoSize = true;
+            this.codigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codigo.Location = new System.Drawing.Point(476, 58);
+            this.codigo.Name = "codigo";
+            this.codigo.Size = new System.Drawing.Size(86, 25);
+            this.codigo.TabIndex = 19;
+            this.codigo.Text = "Código:";
+            // 
+            // menopausa
+            // 
+            this.menopausa.AutoSize = true;
+            this.menopausa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menopausa.Location = new System.Drawing.Point(476, 267);
+            this.menopausa.Name = "menopausa";
+            this.menopausa.Size = new System.Drawing.Size(131, 25);
+            this.menopausa.TabIndex = 18;
+            this.menopausa.Text = "Menopausa:";
+            // 
+            // menarca
+            // 
+            this.menarca.AutoSize = true;
+            this.menarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menarca.Location = new System.Drawing.Point(476, 230);
+            this.menarca.Name = "menarca";
+            this.menarca.Size = new System.Drawing.Size(102, 25);
+            this.menarca.TabIndex = 17;
+            this.menarca.Text = "Menarca:";
+            // 
+            // Peso
+            // 
+            this.Peso.AutoSize = true;
+            this.Peso.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Peso.Location = new System.Drawing.Point(476, 187);
+            this.Peso.Name = "Peso";
+            this.Peso.Size = new System.Drawing.Size(67, 25);
+            this.Peso.TabIndex = 16;
+            this.Peso.Text = "Peso:";
+            // 
+            // dataN
+            // 
+            this.dataN.AutoSize = true;
+            this.dataN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataN.Location = new System.Drawing.Point(476, 150);
+            this.dataN.Name = "dataN";
+            this.dataN.Size = new System.Drawing.Size(215, 25);
+            this.dataN.TabIndex = 15;
+            this.dataN.Text = "Data De Nascimento:";
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome.Location = new System.Drawing.Point(476, 111);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(74, 25);
+            this.Nome.TabIndex = 14;
+            this.Nome.Text = "Nome:";
+            // 
+            // CadastroRosa
+            // 
+            this.CadastroRosa.AutoSize = true;
+            this.CadastroRosa.Font = new System.Drawing.Font("Gloucester MT Extra Condensed", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CadastroRosa.Location = new System.Drawing.Point(857, 389);
+            this.CadastroRosa.Name = "CadastroRosa";
+            this.CadastroRosa.Size = new System.Drawing.Size(196, 41);
+            this.CadastroRosa.TabIndex = 26;
+            this.CadastroRosa.Text = "Cadastro Rosa ++";
+            // 
+            // Atualizar
+            // 
+            this.Atualizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Atualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Atualizar.ForeColor = System.Drawing.Color.Black;
+            this.Atualizar.Location = new System.Drawing.Point(59, 306);
+            this.Atualizar.Name = "Atualizar";
+            this.Atualizar.Size = new System.Drawing.Size(152, 37);
+            this.Atualizar.TabIndex = 30;
+            this.Atualizar.Text = "Atualizar";
+            this.Atualizar.UseVisualStyleBackColor = false;
+            // 
+            // consultar
+            // 
+            this.consultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.consultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consultar.ForeColor = System.Drawing.Color.Black;
+            this.consultar.Location = new System.Drawing.Point(56, 225);
+            this.consultar.Name = "consultar";
+            this.consultar.Size = new System.Drawing.Size(156, 40);
+            this.consultar.TabIndex = 29;
+            this.consultar.Text = "Consultar";
+            this.consultar.UseVisualStyleBackColor = false;
+            // 
+            // ciclo
+            // 
+            this.ciclo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ciclo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ciclo.ForeColor = System.Drawing.Color.Black;
+            this.ciclo.Location = new System.Drawing.Point(58, 151);
+            this.ciclo.Name = "ciclo";
+            this.ciclo.Size = new System.Drawing.Size(155, 40);
+            this.ciclo.TabIndex = 28;
+            this.ciclo.Text = "Ciclo menstrual";
+            this.ciclo.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(58, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 38);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PesoBox
+            // 
+            this.PesoBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.PesoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PesoBox.ForeColor = System.Drawing.Color.Black;
+            this.PesoBox.Location = new System.Drawing.Point(738, 192);
+            this.PesoBox.Mask = "999,9";
+            this.PesoBox.Name = "PesoBox";
+            this.PesoBox.Size = new System.Drawing.Size(272, 26);
+            this.PesoBox.TabIndex = 31;
+            this.PesoBox.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // BtVoltar
+            // 
+            this.BtVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtVoltar.Location = new System.Drawing.Point(66, 370);
+            this.BtVoltar.Name = "BtVoltar";
+            this.BtVoltar.Size = new System.Drawing.Size(144, 35);
+            this.BtVoltar.TabIndex = 32;
+            this.BtVoltar.Text = "Voltar";
+            this.BtVoltar.UseVisualStyleBackColor = true;
+            // 
+            // Cadastrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1085, 450);
+            this.Controls.Add(this.BtVoltar);
+            this.Controls.Add(this.PesoBox);
+            this.Controls.Add(this.Atualizar);
+            this.Controls.Add(this.consultar);
+            this.Controls.Add(this.ciclo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CadastroRosa);
+            this.Controls.Add(this.MenoPausaBox);
+            this.Controls.Add(this.MenarcaBox);
+            this.Controls.Add(this.DataBox);
+            this.Controls.Add(this.NomeBox);
+            this.Controls.Add(this.CodigoBox);
+            this.Controls.Add(this.codigo);
+            this.Controls.Add(this.menopausa);
+            this.Controls.Add(this.menarca);
+            this.Controls.Add(this.Peso);
+            this.Controls.Add(this.dataN);
+            this.Controls.Add(this.Nome);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Cadastrar";
+            this.Text = "Cadastrar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MenoPausaBox;
+        private System.Windows.Forms.TextBox MenarcaBox;
+        private System.Windows.Forms.TextBox DataBox;
+        private System.Windows.Forms.TextBox NomeBox;
+        private System.Windows.Forms.TextBox CodigoBox;
+        private System.Windows.Forms.Label codigo;
+        private System.Windows.Forms.Label menopausa;
+        private System.Windows.Forms.Label menarca;
+        private System.Windows.Forms.Label Peso;
+        private System.Windows.Forms.Label dataN;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label CadastroRosa;
+        private System.Windows.Forms.Button Atualizar;
+        private System.Windows.Forms.Button consultar;
+        private System.Windows.Forms.Button ciclo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox PesoBox;
+        private System.Windows.Forms.Button BtVoltar;
+    }
+}
